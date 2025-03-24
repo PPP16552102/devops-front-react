5b2064f3d5f9bf6356a5982260e2d103e2a3a758
潘宇 <12275622+pan-yu-peter@user.noreply.gitee.com>
2025-03-24 22:15:11 +0800

    fix: api url fix

--- src/api/users.js
+++ src/api/users.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function getUserInfo(params) {
     return axios({
-        url: 'http://localhost:3000/api/users',
+        url: 'http://106.55.164.57:3000/api/users',
         method: 'GET',
         params
     })
